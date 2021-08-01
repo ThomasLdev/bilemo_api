0e59ec3e85834bbf1d16bf4fa6510e7aa59e8216
Thomas LEFEBVRE <tlefebvre@group-dis.com>
2021-08-01 09:39:57 +0200

    fix offset

--- src/EventListener/LinkSerializationSubscriber.php
+++ src/EventListener/LinkSerializationSubscriber.php
@@ -12,9 +12,6 @@ use JMS\Serializer\Metadata\StaticPropertyMetadata;
 use ReflectionObject;
 use Symfony\Component\Routing\RouterInterface;
 
-/**
- * WARNING, THERE IS AN ILLEGAL OFFSET IN THIS CLASS, HAS TO BE FIXED
- */
 class LinkSerializationSubscriber implements EventSubscriberInterface
 {
     private RouterInterface $router;
@@ -43,21 +40,16 @@ class LinkSerializationSubscriber implements EventSubscriberInterface
                 $links[$annotations->name] = $uri;
             }
         }
-        /**
-         * ILLEGAL OFFSET PROBABLY HERE
-         */
         $visitor->visitProperty(new StaticPropertyMetadata(User::class, '_links.self', $links), $links);
     }
 
     public function getSubscribedEvents(): array
     {
         return [
-            [
                 'event' => 'serializer.post_serialize',
                 'method' => 'onPostSerialize',
                 'format' => 'json',
                 'class' => 'App/Entity/User'
-            ]
         ];
     }
 }
