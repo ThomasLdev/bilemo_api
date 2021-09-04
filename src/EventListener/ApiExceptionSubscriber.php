8d4614f8490e5e5db57d7a23997a2dd6cfb7ec2b
Thomas LEFEBVRE <contact@thomas-lefebvre.fr>
2021-09-04 13:12:46 +0200

    adds http code in phone doc

--- src/EventListener/ApiExceptionSubscriber.php
+++ src/EventListener/ApiExceptionSubscriber.php
@@ -47,7 +47,7 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-//            KernelEvents::EXCEPTION => 'onKernelException'
+            KernelEvents::EXCEPTION => 'onKernelException'
         ];
     }
 }
